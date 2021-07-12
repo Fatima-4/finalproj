b44de97c93ca78af7c61e782c4a09e9a8650528c
fatima-4 <afialehfatima@gmail.com>
2021-07-12 21:14:29 +0300

    fatima1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> fatima
+          Edit <code>src/App.js</code> fatima1
         </p>
         <a
           className="App-link"
